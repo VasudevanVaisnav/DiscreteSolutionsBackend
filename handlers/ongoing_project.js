6c8d2193de817722d57cb4c0ec8b2593daaf3b79
bontaramsonta <18cs113@gmail.com>
2020-11-26 13:23:51 +0530

    ongoing project $and issue resolved

--- handlers/ongoing_project.js
+++ handlers/ongoing_project.js
@@ -3,13 +3,12 @@ const mongoose = require("mongoose")
 
 function ongoingProject(req,res,next)
 {
-  Project.find({$and:{isCompleted:false,isApproved:true,emailId:req.params.clientEmailId}}).exec().select("emailId projectTitle problemStatement completionDate startingDate initialDeadline isApproved isCompleted initialBudget finalBudget")
-  .then(docs=>{
+  Project.find({$and:[{isCompleted:false},{isApproved:true},{emailId:req.params.clientEmailId}]}).select("emailId projectTitle problemStatement completionDate startingDate initialDeadline isApproved isCompleted initialBudget finalBudget")
+  .exec().then(doc=>{
     let response = {
       count: docs.length,
       projects: docs.map(doc=>{
         return {
-          count:docs.length,
           ...doc
         }
       })
